dcd6640302c7635b917cc477ebf6a502d12b16f3
张宏伟 <zhanghongwei@zhanghongweidemacbook-pro.local>
2018-10-13 18:18:47 +0800

    Initial Commit

---
+++ testXib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testXib
+//
+//  Created by 张宏伟 on 2018/10/13.
+//  Copyright © 2018年 张宏伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testXib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testXib
+//
+//  Created by 张宏伟 on 2018/10/13.
+//  Copyright © 2018年 张宏伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
