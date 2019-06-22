e24375a437b05e6dc76bf5457dc2fe7129fa439a
[zhanghongwei05] <zhanghongwei05>
2019-06-22 15:27:37 +0800

    merge测试

--- testXib/ViewController.m
+++ testXib/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"zhw test");
+    NSLog(@"zhw merge === 111111");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
